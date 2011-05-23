fe201ae845e11671703271c7da5cf6083f913098
Caleb Brown <cbrown@blenderbox.com>
2011-05-23 07:54:49 -0700

    Added an example of public and private functions.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,6 @@
 var APP = (function($) {
     var app = {};
+    // Public functions
     app.init = function() {
         $.ajaxSetup({
             cache: false,
@@ -8,8 +9,13 @@ var APP = (function($) {
             },
             type: "POST"
         });
+        // See if we have css3 transitions, otherwise add a shim for it.
         if(!Modernizr.csstransitions) { $.getScript('javascripts/css3.js'); }
     };
+    // Private functions
+    function foo() {
+        
+    }
     // Call the init function on load
     $(app.init);
     return app;
