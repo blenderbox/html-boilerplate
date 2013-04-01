aa1fa870d54abda6af069004a0b1aa5bec307021
Caleb Brown <killerdillercaleb@gmail.com>
2013-04-01 12:32:22 -0400

    linted the js plugin and target the nested p tag's margin

--- public/javascripts/simple-plugin-template.jquery.js
+++ public/javascripts/simple-plugin-template.jquery.js
@@ -12,36 +12,36 @@ Call the plugin with $('jquery-selector').pluginName({ foo:'custom-setting' });
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
         var base = this;
-        
+
         // Access to jQuery and DOM versions of element
         base.$el = $(el);
         base.el = el;
-        
+
         // Add a reverse reference to the DOM object
         base.$el.data("pluginName", base);
-        
+
         function init(){
             base.options = $.extend({},$.pluginName.defaults, options);
-            
+
             // Put your initialization code here
-        };
-        
+        }
+
         // Sample Function, Uncomment to use
         // base.functionName = function(paramaters){
         // 
         // };
-        
+
         // Run initializer
         init();
     };
 
     // default settings
     $.pluginName.defaults = { };
-    
+
     $.fn.pluginName = function(options){
         return this.each(function(){
             (new $.pluginName(this, options));
         });
     };
-    
+
 })(jQuery);
