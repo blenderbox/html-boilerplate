786df4309b6c6748bc2b191ce4617ca50a83f990
Caleb Brown <killerdillercaleb@gmail.com>
2013-10-15 18:39:36 -0400

    updated the simple plugin

--- public/javascripts/simple-plugin-template.jquery.js
+++ public/javascripts/simple-plugin-template.jquery.js
@@ -8,13 +8,13 @@ Call the plugin with $('jquery-selector').pluginName({ foo:'custom-setting' });
 */
 
 (function($){
-    $.pluginName = function(el, options){
+    $.pluginName = function(el, $el, options){
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
         var base = this;
 
         // Access to jQuery and DOM versions of element
-        base.$el = $(el);
+        base.$el = $el;
         base.el = el;
 
         // Add a reverse reference to the DOM object
@@ -28,7 +28,7 @@ Call the plugin with $('jquery-selector').pluginName({ foo:'custom-setting' });
 
         // Sample Function, Uncomment to use
         // base.functionName = function(paramaters){
-        // 
+        //
         // };
 
         // Run initializer
@@ -38,10 +38,25 @@ Call the plugin with $('jquery-selector').pluginName({ foo:'custom-setting' });
     // default settings
     $.pluginName.defaults = { };
 
+    // our jQuery plugin magic.
     $.fn.pluginName = function(options){
-        return this.each(function(){
-            (new $.pluginName(this, options));
+
+        return this.each(function() {
+            var $el = $(this),
+                key = 'pluginName';
+
+            // Return early if this element already has a plugin instance
+            if ($el.data(key)) {
+                return $el.data(key);
+            }
+
+            // Pass options to plugin constructor
+            var pluginName = (new $.pluginName(this, $el, options));
+
+            // Store plugin object in this element's data
+            $el.data(key, pluginName);
         });
+
     };
 
 })(jQuery);
