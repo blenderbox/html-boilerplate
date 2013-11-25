973fbfea06813deac5ffe8da1fdbc713a0614663
Caleb Brown <killerdillercaleb@gmail.com>
2013-11-25 10:02:54 -0500

    adding a debounce script and cleaning up to latest formatting

--- public/javascripts/app/application.js
+++ public/javascripts/app/application.js
@@ -1,62 +1,79 @@
+//= require ../libs/debounce
 //= require ../libs/log
 
 var APP = (function($, undefined) {
-    'use strict';
+  'use strict';
 
-    var app = {},
-        $el;
+  var app = {},
+    $el;
 
-    // public functions
-    // app.foo = function() {  };
+  // public functions
+  // app.foo = function() {  };
 
-    // private functions
-    function init() {
-        $('a[href=#]').attr('href', 'javascript:;');
+  // private functions
+  function init() {
+    $('a[href=#]').attr('href', 'javascript:;');
 
-        // Open links starting with "http(s)://" in a new window unless they're targeted at this host.
-        $("a[href^=http]").click(open);
+    // Open links starting with "http(s)://" in a new window unless they're targeted at this host.
+    $("a[href^=http]").click(open);
 
-        // Set up the global ajax
-        $.ajaxSetup({ cache: false, error: function errorLog(x, e) { log(x, e); }, type: 'POST' });
-
-        if (!Modernizr.input.placeholder) { placeholder(); }
-
-        /*
-        yepnope([{
-            test:Modernizr.csstransitions,
-            nope:'/javascripts/app/css3.js'
-        }]);
-        */
-    }
-    function open(e) {
-        e.preventDefault();
-        var href = this.getAttribute("href");
-        if (window.location.host !== href.split('/')[2]) {
-            window.open(href);
-        } else {
-            window.location = href;
+    // Set up the global ajax
+    $.ajaxSetup({
+      cache: false,
+      error: function errorLog(x, e) {
+        if (typeof log !== 'undefined') {
+          log(x, e);
         }
+      },
+      type: 'POST'
+    });
+
+    if (!Modernizr.input.placeholder) {
+      placeholder();
     }
-    function placeholder() {
-        var attr = 'placeholder';
-        $('input[' + attr + '!=""]').each(function(idx, el){
-            $el = $(el);
-            var d = $el.attr(attr);
-            if (d === undefined || $el.attr('type') === 'password') { return; }
-            $el
-                .focus(function onFocus() {
-                    $(this).removeClass(attr);
-                    if (this.value === d) { this.value = ''; }
-                })
-                .blur(function onBlur() {
-                    if ($.trim(this.value) === '') {
-                        $(this).addClass(attr);
-                        this.value = d;
-                    }
-                }).blur();
-        });
+
+    /*
+    Yepnope is available through Modernizr.
+
+    Yepnope example usage:
+
+    yepnope([{
+      test:Modernizr.csstransitions,
+      nope:'/javascripts/app/css3.js'
+    }]);
+    */
+  }
+
+  function open(e) {
+    e.preventDefault();
+    var href = this.getAttribute("href");
+    if (window.location.host !== href.split('/')[2]) {
+      window.open(href);
+    } else {
+      window.location = href;
     }
-    // Call the init function on load
-    $(init);
-    return app;
+  }
+
+  function placeholder() {
+    var attr = 'placeholder';
+    $('input[' + attr + '!=""]').each(function(idx, el){
+      $el = $(el);
+      var d = $el.attr(attr);
+      if (d === undefined || $el.attr('type') === 'password') { return; }
+      $el
+        .focus(function onFocus() {
+          $(this).removeClass(attr);
+          if (this.value === d) { this.value = ''; }
+        })
+        .blur(function onBlur() {
+          if ($.trim(this.value) === '') {
+            $(this).addClass(attr);
+            this.value = d;
+          }
+        }).blur();
+    });
+  }
+  // Call the init function on load
+  $(init);
+  return app;
 } (jQuery));
--- public/javascripts/simple-plugin-template.jquery.js
+++ public/javascripts/simple-plugin-template.jquery.js
@@ -8,48 +8,48 @@ Call the plugin with $('jquery-selector').pluginName({ foo:'custom-setting' });
 */
 
 ;(function($, undefined){
-    $.pluginName = function(el, $el, options){
-        // To avoid scope issues, use 'base' instead of 'this'
-        // to reference this class from internal events and functions.
-        var base = this;
+  $.pluginName = function(el, $el, options){
+    // To avoid scope issues, use 'base' instead of 'this'
+    // to reference this class from internal events and functions.
+    var base = this;
 
-        // Access to jQuery and DOM versions of element
-        base.$el = $el;
-        base.el = el;
+    // Access to jQuery and DOM versions of element
+    base.$el = $el;
+    base.el = el;
 
-        function init(){
-            base.options = $.extend({},$.pluginName.defaults, options);
+    function init(){
+      base.options = $.extend({},$.pluginName.defaults, options);
 
-            // Put your initialization code here
-        }
+      // Put your initialization code here
+    }
 
-        // Sample Function, Uncomment to use
-        // base.functionName = function(paramaters){
-        //
-        // };
+    // Sample Function, Uncomment to use
+    // base.functionName = function(paramaters){
+    //
+    // };
 
-        // Run initializer
-        init();
-    };
+    // Run initializer
+    init();
+  };
 
-    // default settings
-    $.pluginName.defaults = { };
+  // default settings
+  $.pluginName.defaults = { };
 
-    // our jQuery plugin magic.
-    $.fn.pluginName = function(options){
+  // our jQuery plugin magic.
+  $.fn.pluginName = function(options){
 
-        return this.each(function(idx, el) {
-            var $el = $(el),
-                key = 'pluginName';
+    return this.each(function(idx, el) {
+      var $el = $(el),
+        key = 'pluginName';
 
-            // Only create the plugin instance if it doesn't exist
-            if (!$el.data(key)) {
-                // Store plugin object in this element's data
-                $el.data(key, new $.pluginName(el, $el, options));
-            }
+      // Only create the plugin instance if it doesn't exist
+      if (!$el.data(key)) {
+        // Store plugin object in this element's data
+        $el.data(key, new $.pluginName(el, $el, options));
+      }
 
-        });
+    });
 
-    };
+  };
 
 })(jQuery);
